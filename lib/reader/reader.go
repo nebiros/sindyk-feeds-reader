28c78bab9223abf23a41a34797f11f4f92011c66
Juan Felipe Alvarez Saldarriaga <nebiros@gmail.com>
2015-05-29 10:21:02 -0500

    WIP: sql stuff.

--- lib/reader/reader.go
+++ lib/reader/reader.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"runtime"
 	"fmt"
-	"github.com/nebiros/sindyk-feeds-reader/lib/rss"
+	"strings"
+	"html"
+	"net/url"
 )
 
 const (
@@ -17,9 +19,9 @@ const (
 
 var (
 	// db connection.
-	DB *sql.DB
+	conn *sql.DB
 	// feeds channel.
-	feedsChannel chan rss.Rss
+	feedsChannel chan feedChannelPack
 )
 
 type Params struct {
@@ -32,27 +34,55 @@ type FeedRow struct {
 	Active bool
 }
 
+type ItemRow struct {
+	FeedId int
+	ExternalId int
+	Title string
+	Description string
+	Link string
+	PubDate string
+	Content string
+	Creator string
+	ImageUrl string
+	Active bool
+	DisplayOrder int
+	Subject string
+	Category string
+	Hour string
+	Related string
+	Slug string
+}
+
+type feedChannelPack struct {
+	rss *Rss
+	feedRow *FeedRow
+	err error
+}
+
 func Start(p Params) {
 	OpenDb(p)
 
-	// get all feeds from db.
-	feeds := Feeds()
+	// get all feeds from conn.
+	activeFeeds := ActiveFeedsFromDb()
 	// process each feed.
-	Process(feeds)
+	Process(activeFeeds)
+
+	defer conn.Close()
 }
 
 func OpenDb(p Params) {
 	var err error
 
-	dsn := buildDbDsn(p)
-	DB, err = sql.Open(DbDefaultDriver, dsn)
+	conn, err = sql.Open(DbDefaultDriver, buildDbDsn(p))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("[OpenDb] " + err.Error())
+		conn.Close()
 	}
 
-	err = DB.Ping()
+	err = conn.Ping()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("[OpenDb] " + err.Error())
+		conn.Close()
 	}
 }
 
@@ -68,7 +98,7 @@ func buildDbDsn(p Params) string {
 	return dsn
 }
 
-func Feeds() (feeds []FeedRow) {
+func ActiveFeedsFromDb() (activeFeeds []*FeedRow) {
 	query := `SELECT
 		feeds.id AS feed_id,
 		feeds.active AS feed_active,
@@ -84,53 +114,272 @@ func Feeds() (feeds []FeedRow) {
 		WHERE
 		feeds.active = ?`
 
-	stmt, err := DB.Prepare(query)
+	rows, err := conn.Query(query, 1, 0, 1, 1)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(1, 0, 1, 1)
-	if err != nil {
-		log.Fatal(err)
+		log.Fatal("[ActiveFeedsFromDb] " + err.Error())
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		f := FeedRow{}
+		f := new(FeedRow)
 
 		err := rows.Scan(&f.Id, &f.Active, &f.Url)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("[ActiveFeedsFromDb] " + err.Error())
 		}
 
-		feeds = append(feeds, f)
+		activeFeeds = append(activeFeeds, f)
 	}
 	err = rows.Err()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("[ActiveFeedsFromDb] " + err.Error())
 	}
 
-	defer DB.Close()
-
-	return feeds
+	return activeFeeds
 }
 
-func Process(feeds []FeedRow) {
+func Process(af []*FeedRow) {
 	_ = "breakpoint"
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	for _, f := range feeds {
-		go rss.Fetch(f.Url, rssHandler)
+	for _, f := range af {
+		go FetchRss(f.Url, f, rssHandler)
 	}
 
-	feedsChannel = make(chan rss.Rss)
-	for r := range feedsChannel {
-		fmt.Printf("[PullFeeds] r, %T, %#v\n", r, r)
+	feedsChannel = make(chan feedChannelPack)
+	for cp := range feedsChannel {
+		// fmt.Printf("[PullFeeds] r, %T, %#v\n\n", cp, cp)
+		if cp.err != nil {
+			log.Println("[Process] " + cp.err.Error())
+		} else {
+			for _, i := range cp.rss.RssItemList {
+				// item := cp.rss.RssItemList[l]
+				fmt.Printf("[RssItemList] item, %T, %#v\n\n", i, i)
+				content := i.Content
+				if len(content) <= 0 {
+					content = i.Description
+				}
+
+				subject := strings.TrimSpace(i.Subject)
+				if len(subject) <= 0 {
+					subject = strings.TrimSpace(i.DcSubject)
+				}
+
+				creator := i.Creator
+				if len(creator) <= 0 {
+					creator = i.DcCreator
+				}
+
+				var imageUrl string
+				if i.RssItemEnclosure != nil {
+					mime := strings.Split(i.RssItemEnclosure.MimeType, "/")
+					if mime[0] == "image" {
+						imageUrl = i.RssItemEnclosure.Url
+					}
+				}
+
+				link := strings.TrimSpace(i.Link)
+
+				u, err := url.Parse(link)
+			    if err != nil {
+			        log.Println("[Process] " + err.Error())
+			    }
+
+				ir := ItemRow{FeedId: cp.feedRow.Id,
+					ExternalId: i.Id,
+					Title: strings.TrimSpace(i.Title),
+					Description: html.EscapeString(i.Description),
+					Link: link,
+					PubDate: strings.TrimSpace(i.PubDate),
+					Content: html.EscapeString(content),
+					Creator: html.EscapeString(creator),
+					ImageUrl: imageUrl,
+					Active: true,
+					DisplayOrder: i.Order,
+					Category: strings.TrimSpace(i.Category),
+					Subject: subject,
+					Hour: strings.TrimSpace(i.Hour),
+					Related: strings.TrimSpace(i.Related),
+					Slug: u.Path}
+				// SaveItemToDb(&ir)
+			}
+		}
 	}
 }
 
-func rssHandler(rss rss.Rss, err error) {
-	feedsChannel <- rss
+func rssHandler(r *Rss, fr *FeedRow, err error) {
+	// DisableFeedItemsFromDb(fr.Id)
+	fmt.Printf("[rssHandler] fr, %T, %#v\n\n", fr, fr)
+	feedsChannel <- feedChannelPack{rss: r, feedRow: fr, err: err}
+}
+
+func SaveItemToDb(ir *ItemRow) {
+	var (
+		itemIdSelectQuery string
+		itemId *int
+	)
+	_ = "breakpoint"
+	if ir.ExternalId > 0 {
+		itemIdSelectQuery = `SELECT
+            items.id
+            FROM
+            items
+            WHERE
+            items.external_id = ?
+            AND
+            items.feed_id = ?`
+
+		err := conn.QueryRow(itemIdSelectQuery, ir.ExternalId, ir.FeedId).Scan(&itemId)
+		if err != nil {
+			if err != sql.ErrNoRows {
+				log.Fatal("[SaveItemToDb] " + err.Error())
+			}
+		}
+	} else {
+		itemIdSelectQuery = `SELECT
+		    items.id
+		    FROM
+		    items
+		    WHERE
+		    items.title LIKE ?
+		    AND
+		    items.feed_id = ?`
+
+		err := conn.QueryRow(itemIdSelectQuery, fmt.Sprintf("%%s%", ir.Title), ir.FeedId).Scan(itemId)
+		if err != nil {
+			if err != sql.ErrNoRows {
+				log.Fatal("[SaveItemToDb] " + err.Error())
+			}
+		}
+	}
+	fmt.Printf("[SaveItemToDb] itemId, %T, %#v\n\n", itemId, itemId)
+
+	tx, err := conn.Begin()
+	if err != nil {
+		log.Fatal("[SaveItemToDb] " + err.Error())
+	}
+	defer tx.Rollback()
+
+	if itemId != nil {
+		updateQuery := `UPDATE items SET external_id = ?,
+			feed_id = ?,
+			title = ?,
+			link = ?,
+			pubdate = ?,
+			creator = ?,
+			display_order = ?,
+			subject = ?,
+			category = ?,
+			description = ?,
+			content = ?,
+			image = ?,
+			hour = ?,
+			relacionadas = ?,
+			active = ?
+			WHERE
+			id = ?`
+
+		stmt, err := tx.Prepare(updateQuery)
+		if err != nil {
+			log.Fatal("[SaveItemToDb] " + err.Error())
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(ir.ExternalId,
+			ir.FeedId,
+			ir.Title,
+			ir.Link,
+			ir.PubDate,
+			ir.Creator,
+			ir.DisplayOrder,
+			ir.Subject,
+			ir.Category,
+			ir.Description,
+			ir.Content,
+			ir.ImageUrl,
+			ir.Hour,
+			ir.Related,
+			ir.Active,
+			itemId)
+		if err != nil {
+			log.Fatal("[SaveItemToDb] " + err.Error())
+		}
+	} else {
+		insertQuery := `INSERT INTO items (external_id,
+			feed_id,
+			title,
+			link,
+			pubdate,
+			creator,
+			display_order,
+			subject,
+			category,
+			description,
+			content,
+			image,
+			hour,
+			relacionadas,
+			active)
+			VALUES
+			(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
+		stmt, err := tx.Prepare(insertQuery)
+		if err != nil {
+			log.Fatal("[SaveItemToDb] " + err.Error())
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(ir.ExternalId,
+			ir.FeedId,
+			ir.Title,
+			ir.Link,
+			ir.PubDate,
+			ir.Creator,
+			ir.DisplayOrder,
+			ir.Subject,
+			ir.Category,
+			ir.Description,
+			ir.Content,
+			ir.ImageUrl,
+			ir.Hour,
+			ir.Related,
+			ir.Active)
+		if err != nil {
+			log.Fatal("[SaveItemToDb] " + err.Error())
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal("[SaveItemToDb] " + err.Error())
+	}
+}
+
+func DisableFeedItemsFromDb(id int) {
+	tx, err := conn.Begin()
+	if err != nil {
+		log.Fatal("[DisableFeedItemsFromDb] " + err.Error())
+	}
+	defer tx.Rollback()
+
+	updateQuery := `UPDATE items SET active = 0
+		WHERE
+		feed_id = ?`
+
+	stmt, err := tx.Prepare(updateQuery)
+	if err != nil {
+		log.Fatal("[DisableItemsFromDb] " + err.Error())
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(id)
+	if err != nil {
+		log.Fatal("[DisableItemsFromDb] " + err.Error())
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal("[DisableItemsFromDb] " + err.Error())
+	}
 }
---
+++ lib/reader/rss.go
@@ -0,0 +1,112 @@
+package reader
+
+import (
+	"io/ioutil"
+	"encoding/xml"
+	"net/http"
+	"errors"
+	"bytes"
+
+	"github.com/nebiros/sindyk-feeds-reader/lib/charset"
+)
+
+type Rss struct {
+	XMLName xml.Name `xml:"rss"`
+	Version string `xml:"version,attr"`
+	// Required
+	Title string `xml:"channel>title"`
+	Link string `xml:"channel>link"`
+	Description string `xml:"channel>description"`
+	// Optional
+	PubDate string `xml:"channel>pubDate"`
+	RssItemList []*RssItem `xml:"channel>item"`
+}
+
+type RssItem struct {
+	// Required
+	Title string `xml:"title"`
+	Link string `xml:"link"`
+	Description string `xml:"description"`
+	// Optional
+	Content string `xml:"encoded,omitempty"`
+	PubDate string `xml:"pubDate,omitempty"`
+	Comments string `xml:"comments,omitempty"`
+	Guid string `xml:"guid,omitempty"`
+	DcSubject string `xml:"http://purl.org/dc/elements/1.1/ dc:subject,omitempty"`
+	DcCreator string `xml:"http://purl.org/dc/elements/1.1/ dc:creator,omitempty"`
+	RssItemEnclosure *RssItemEnclosure `xml:"enclosure,omitempty"`
+	Category string `xml:"category,omitempty"`
+	Hour string `xml:"hora,omitempty"`
+	Order int `xml:"order,omitempty"`
+	Id int `xml:"id,omitempty"`
+	Related string `xml:"relacionadas,omitempty"`
+	Subject string `xml:"subject,omitempty"`
+	Creator string `xml:"creator,omitempty"`
+}
+
+type RssItemEnclosure struct {
+	Url string `xml:"url,attr"`
+	MimeType string `xml:"type,attr"`
+}
+
+type RssHandlerFunc func (rss *Rss, fr *FeedRow, err error)
+
+func FetchRss(uri string, fr *FeedRow, rh RssHandlerFunc) {
+	b, err := LoadRssUri(uri)
+	if err != nil {
+		if rh != nil {
+			rh(nil, nil, err)
+		}
+		return
+	}
+
+	f, err := ParseRss(b)
+	if err != nil {
+		if rh != nil {
+			rh(nil, nil, err)
+		}
+		return
+	}
+
+	if rh != nil {
+		rh(f, fr, nil)
+	}
+}
+
+func LoadRssUri(uri string) (content []byte, err error) {
+	client := http.DefaultClient
+	resp, err := client.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(data), nil
+}
+
+func ParseRss(c []byte) (rss *Rss, err error) {
+	r := new(Rss)
+
+	decoder := xml.NewDecoder(bytes.NewReader(c))
+	decoder.CharsetReader = charset.CharsetReader
+	err = decoder.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+
+	if r.Version != "2.0" {
+		return nil, errors.New("Not a valid RSS 2.0 feed")
+	}
+
+	for i, _ := range r.RssItemList {
+		if r.RssItemList[i].Content != "" {
+			r.RssItemList[i].Description = r.RssItemList[i].Content
+		}
+	}
+	return r, nil
+}
